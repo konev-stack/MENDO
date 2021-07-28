532a0213952eb8812b4e9adfc0a1046525baf1b1
kone :) <85243178+konev-stack@users.noreply.github.com>
2021-07-28 13:44:51 +0200

    Create kruzhnica.cpp

---
+++ kruzhnica/kruzhnica.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int r;
+    cin >> r;
+    float p = 3.1415;
+    double l;
+    double area;
+ 
+    l = 2 * r * p;
+    area = r * r * p;
+ 
+    cout << l << " " << area;
+ 
+ 
+    return 0;
+}
