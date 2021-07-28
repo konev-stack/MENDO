ffc5be4f0ea349b4e2878910396ce4c7ccc4bff6
kone :) <85243178+konev-stack@users.noreply.github.com>
2021-07-28 13:57:12 +0200

    Create meseci.cpp

---
+++ meseci/meseci.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+    int month;
+    cin >> month;
+ 
+    if (month == 1)
+    {
+        cout << "31" << endl;
+    }
+    else if (month == 2)
+    {
+        cout << "28" << endl;
+    }
+    else if (month == 3)
+    {
+        cout << "31" << endl;
+    }
+    else if (month == 4)
+    {
+        cout << "30" << endl;
+    }
+    else if (month == 5)
+    {
+        cout << "31" << endl;
+    }
+    else if (month == 6)
+    {
+        cout << "30" << endl;
+    }
+    else if (month == 7)
+    {
+        cout << "31" << endl;
+    }
+    else if (month == 8)
+    {
+        cout << "31" << endl;
+    }
+    else if (month == 9)
+    {
+        cout << "30" << endl;
+    }
+    else if (month == 10)
+    {
+        cout << "31" << endl;
+ 
+    }
+    else if (month == 11)
+    {
+        cout << "30" << endl;
+    }
+    else if (month == 12)
+    {
+        cout << "31" << endl;
+    }
+    else
+    {
+        cout << "Greska";
+    }
+ 
+    return 0;
+ 
+}
